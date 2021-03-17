4c573891b59533952c7112329cd64f1ebcb48613
Manash <manashbharali79@gmail.com>
2021-03-17 22:58:56 +0530

    Database seeder added for Users,Customer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,108 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		\App\Models\User::factory(5)->create();
+
+		\App\Models\Customer::create([
+			'user_id' => 1,
+			'name' => 'John Doe',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 2,
+			'name' => 'Sara Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 2,
+			'name' => 'Ronn smith',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 3,
+			'name' => 'Sara Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 1,
+			'name' => 'Jayden Doe',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 2,
+			'name' => 'Rock Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 2,
+			'name' => 'Json smith',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 3,
+			'name' => 'Hunter Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+		\App\Models\Customer::create([
+			'user_id' => 1,
+			'name' => 'Luke Doe',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 5,
+			'name' => 'Lone Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 2,
+			'name' => 'Brad smith',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+
+		\App\Models\Customer::create([
+			'user_id' => 4,
+			'name' => 'Rony Jane',
+			'contact' => '9876543212',
+			'address' => 'Some address xyz street',
+			'status' => 1,
+		]);
+	}
 }
